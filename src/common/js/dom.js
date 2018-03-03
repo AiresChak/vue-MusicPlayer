28ce7e6b2e74b2a75f4d8ce0bb4293cc48caef3e
ZoeChak <278576149@qq.com>
2018-03-03 18:25:13 +0800

    create singer page

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,5 +1,5 @@
-export function addClass(el,className){
-  if(hasClass(el,className)){
+export function addClass (el, className) {
+  if (hasClass (el, className) ) {
     return;
   }
   let newClass = el.className.split(' ');
@@ -7,7 +7,7 @@ export function addClass(el,className){
   el.className = newClass.join(' ');
 }
 
-export function hasClass(el,className){
+export function hasClass (el, className) {
   let reg = new RegExp('(^|\\s)'+className+'(\\s|$)');
   return reg.test(el.className);
 }
